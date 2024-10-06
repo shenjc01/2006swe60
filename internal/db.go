8de5a4ad032de6dd3cef898b8b0ba822bfea2fcc
Wei Yu <104413257+firepiratex@users.noreply.github.com>
2024-10-06 13:25:52 +0800

    change from sqlite3 to sqlite

--- internal/db.go
+++ internal/db.go
@@ -4,7 +4,8 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/mattn/go-sqlite3"
+	//_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
@@ -12,7 +13,7 @@ var DB *sql.DB
 // Initialize the database connection
 func InitDB() {
 	var err error
-	DB, err = sql.Open("sqlite3", "./db/data.db")
+	DB, err = sql.Open("sqlite", "./db/data.db")
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
