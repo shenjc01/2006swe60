65ef8836bcd41893d9a17203597282699384c353
Wei Yu <104413257+firepiratex@users.noreply.github.com>
2024-10-16 14:56:43 +0800

    updated the registerUser handle

--- internal/handler.go
+++ internal/handler.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
 // RegisterUser handles user registration requests
@@ -15,15 +16,30 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Parse the form data
-	err := r.ParseForm()
+	
+	// Parse the JSON body
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Email string `json:"email"`
+	}
+	
+	// Decode the JSON Body
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		fmt.Println(err)
+		w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusBadRequest)
+        json.NewEncoder(w).Encode(map[string]interface{}{
+            "success": false,
+            "message": "Invalid request body",
+        })
 		return
 	}
-	username := r.FormValue("username")
-	password := r.FormValue("password")
+	
+	username := req.Username
+	password := req.Password
+	email := req.Email
 
 	salt := make([]byte, 4)
 	_, err = rand.Read(salt)
@@ -40,12 +56,22 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Insert the new user into the database
-	_, err = DB.Exec("INSERT INTO Users (username, hashedPassword, salt) VALUES (?, ?, ?)", username, hex.EncodeToString(hashedPassword), hex.EncodeToString(salt))
+	_, err = DB.Exec("INSERT INTO Users (username, hashedPassword, salt, email) VALUES (?, ?, ?, ?)", username, hex.EncodeToString(hashedPassword), hex.EncodeToString(salt), email)
 	if err != nil {
 		log.Printf("SQL Insert Error: %v", err)
-		http.Error(w, "Username already taken or SQL error: "+err.Error(), http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusBadRequest)
+        json.NewEncoder(w).Encode(map[string]interface{}{
+            "success": false,
+            "message": "Username already taken or SQL error",
+        })
 		return
 	}
 
-	w.Write([]byte("User registered successfully!"))
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(map[string]interface{}{
+        "success": true,
+        "message": "User registered successfully!",
+    })
 }
