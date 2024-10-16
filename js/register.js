89ef1fd30372c5b4a20bac352e03af198c99049a
Wei Yu <104413257+firepiratex@users.noreply.github.com>
2024-10-17 01:15:45 +0800

    updated register.js
    to encrypt the info being sent to the backend

--- js/register.js
+++ js/register.js
@@ -9,10 +9,17 @@ async function onSubmit(event){
 		alert("Passwords do not match!");
 		return;
 	}
+	console.log("Starting handshake");
+    const aesKey = await startup();
+    console.log("Completed handshake");
+    const sessionID = sessionStorage.getItem("sessionID");
+    const {ciphertext, iv} = await encrypt(aesKey, password);
     const payload = {
-        username: username,
-        password: password,
-		email: email,
+        sessionid: sessionID,
+        username: username, // Send the username as a string
+        ciphertext: ciphertext, // Convert ciphertext to base64
+        iv: iv, // Convert IV to base64
+		email: email
     };
     let response = await fetch('/registerProcess', {
         method: 'POST',
